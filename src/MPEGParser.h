464e94b6ecc5a533eb5c3ab06d6176565e8cb698
cantabile <cantabile.desu@gmail.com>
2017-04-11 14:10:48 +0300

    Make the MPEG2 parser fill in AVCodecParserContext
    Instead of using a custom struct. This way some duplicated code can be
    removed from D2V::handleVideoPacket.
    
    We now pretend all GOPs are open in MPEG2 as well. And that progressive
    sequences only happen when the RFF flag is used. But these are not very
    important details.

--- src/MPEGParser.h
+++ src/MPEGParser.h
@@ -24,64 +24,12 @@ SOFTWARE.
 
 #include <cstdint>
 
-
-struct MPEGParser {
-    enum PictureCodingType {
-        I_PICTURE = 1,
-        P_PICTURE = 2,
-        B_PICTURE = 3
-    };
-
-    enum MatrixCoefficients {
-        MATRIX_FORBIDDEN = 0,
-        MATRIX_BT709 = 1,
-        MATRIX_UNSPECIFIED = 2,
-        MATRIX_RESERVED = 3,
-        MATRIX_FCC = 4,
-        MATRIX_BT470BG = 5,
-        MATRIX_SMPTE170M = 6,
-        MATRIX_SMPTE240M = 7
-    };
+extern "C" {
+#include <libavcodec/avcodec.h>
+}
 
 
-    int width;
-    int height;
-    int picture_coding_type;
-    bool progressive_sequence;
-    bool top_field_first;
-    bool repeat_first_field;
-    bool progressive_frame;
-    bool group_of_pictures_header;
-    bool closed_gop;
-    uint8_t matrix_coefficients;
+void d2vWitchParseMPEG12Data(AVCodecParserContext *parser, AVCodecContext *avctx, const uint8_t *data, int data_size);
 
-
-    MPEGParser();
-
-    void parseData(const uint8_t *data, int data_size);
-
-
-private:
-
-    enum StartCodes {
-        PICTURE_START_CODE = 0x00,
-        SEQUENCE_HEADER_CODE = 0xb3,
-        EXTENSION_START_CODE = 0xb5,
-        GROUP_START_CODE = 0xb8,
-        SLICE_START_CODE_MIN = 0x01,
-        SLICE_START_CODE_MAX = 0xaf
-    };
-
-    enum ExtensionStartCodeIdentifier {
-        SEQUENCE_EXTENSION = 0x1,
-        SEQUENCE_DISPLAY_EXTENSION = 0x2,
-        PICTURE_CODING_EXTENSION = 0x8
-    };
-
-
-    void clear();
-
-    const uint8_t *findStartCode(const uint8_t *data, const uint8_t *data_end, uint32_t *start_code);
-};
 
 #endif // D2V_WITCH_MPEGPARSER_H
